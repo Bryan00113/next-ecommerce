3af49a68ff611258c8f89d628b5bb82cbe2bd310
Bryandev1920 <73914327+Bryan00113@users.noreply.github.com>
2025-03-20 16:28:08 -0600

    eliminando archivos
    Eliminando el talwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,23 +9,13 @@ const config: Config = {
   theme: {
     screens: {
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       md: "768px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-      smhero: "540px",
-      mdHero: "940px",
-      hero: "1040px",
-      lg2: "1108px",
-      // => @media (min-width: 640px) { ... }
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
+      // Breakpoints personalizados
+      mdhero: "846px", // Usar max-width en lugar de min-width
+      // smhero: { min: "846px", max: "2023px" },
     },
     extend: {
       backgroundImage: {
@@ -35,6 +25,36 @@ const config: Config = {
       },
       colors: {
         lama: "#f35c7a",
+        border: "hsl(var(--border) / <alpha-value>)",
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
+        primary: "hsl(var(--primary) / <alpha-value>)",
+        "primary-foreground": "hsl(var(--primary-foreground) / <alpha-value>)",
+        secondary: "hsl(var(--secondary) / <alpha-value>)",
+        "secondary-foreground":
+          "hsl(var(--secondary-foreground) / <alpha-value>)",
+        muted: "hsl(var(--muted) / <alpha-value>)",
+        "muted-foreground": "hsl(var(--muted-foreground) / <alpha-value>)",
+        accent: "hsl(var(--accent) / <alpha-value>)",
+        "accent-foreground": "hsl(var(--accent-foreground) / <alpha-value>)",
+        destructive: "hsl(var(--destructive) / <alpha-value>)",
+        "destructive-foreground":
+          "hsl(var(--destructive-foreground) / <alpha-value>)",
+        card: "hsl(var(--card) / <alpha-value>)",
+        "card-foreground": "hsl(var(--card-foreground) / <alpha-value>)",
+        popover: "hsl(var(--popover) / <alpha-value>)",
+        "popover-foreground": "hsl(var(--popover-foreground) / <alpha-value>)",
+        input: "hsl(var(--input) / <alpha-value>)",
+        ring: "hsl(var(--ring) / <alpha-value>)",
+
+        // Añadir el color personalizado dashboard-dark
+        "dashboard-dark": "#121212", // Puedes ajustar este valor según tus necesidades
+        "dashboard-purple": "hsl(var(--primary) / <alpha-value>)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
